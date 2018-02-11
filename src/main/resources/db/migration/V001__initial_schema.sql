3a082c769fc480599dc4d4efbd278824c675b382
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-02-11 21:36:49 +0000

    Adding timestamps

--- src/main/resources/db/migration/V001__initial_schema.sql
+++ src/main/resources/db/migration/V001__initial_schema.sql
@@ -2,7 +2,8 @@ create table accounts(
     id varchar(36) primary key,
     account_number varchar(8) not null,
     sort_code varchar(6) not null,
-    balance number(10,3) not null
+    balance number(10,3) not null,
+    opened_at timestamp not null
 );
 
 create table transactions(
@@ -10,6 +11,7 @@ create table transactions(
     account_id varchar(36) not null,
     amount number(10,3) not null,
     type varchar(32) not null,
+    issued_at timestamp not null,
 
     foreign key (account_id) references accounts (id)
 );
