2b3bc75f65aae83b752a347ed12515bf17ceafbb
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-02-11 18:18:51 +0000

    Externally modifiable config

--- src/main/java/com/revolut/tests/zkiss/transfersvc/TransferService.java
+++ src/main/java/com/revolut/tests/zkiss/transfersvc/TransferService.java
@@ -1,8 +1,10 @@
 package com.revolut.tests.zkiss.transfersvc;
 
 import lombok.extern.slf4j.Slf4j;
+import ratpack.func.Action;
 import ratpack.handling.RequestLogger;
 import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfigBuilder;
 
 @Slf4j
 public class TransferService {
@@ -12,18 +14,24 @@ public class TransferService {
     }
 
     public static RatpackServer server() throws Exception {
-        return RatpackServer.of(server -> server
-                .registryOf(registry -> registry.add("World!"))
-                .serverConfig(config -> config
-                        .yaml(ClassLoader.getSystemResource("config.yaml"))
-                )
-                .handlers(root -> root
-                        .all(RequestLogger.ncsa())
-                        .prefix("transfers", transfers -> transfers
-                                .post(ctx -> ctx.render("Creates new tx"))
-                                .get(":id", ctx -> ctx.render("Return tx " + ctx.getPathTokens().get("id")))
-                        )
-                )
+        return server(Action.noop());
+    }
+
+    public static RatpackServer server(Action<ServerConfigBuilder> configure) throws Exception {
+        Action<ServerConfigBuilder> configureDefault = config -> config
+                .yaml(ClassLoader.getSystemResource("config.yaml"));
+        return RatpackServer.of(server -> {
+                    server
+                            .registryOf(registry -> registry.add("World!"))
+                            .serverConfig(configureDefault.append(configure))
+                            .handlers(root -> root
+                                    .all(RequestLogger.ncsa())
+                                    .prefix("transfers", transfers -> transfers
+                                            .post(ctx -> ctx.render("Creates new tx"))
+                                            .get(":id", ctx -> ctx.render("Return tx " + ctx.getPathTokens().get("id")))
+                                    )
+                            );
+                }
         );
     }
 }
--- src/test/java/com/revolut/tests/zkiss/transfersvc/CreateTransferIT.java
+++ src/test/java/com/revolut/tests/zkiss/transfersvc/CreateTransferIT.java
@@ -1,5 +1,7 @@
 package com.revolut.tests.zkiss.transfersvc;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ratpack.test.embed.EmbeddedApp;
 
@@ -7,12 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateTransferIT {
 
+    private EmbeddedApp app;
+
+    @Before
+    public void setUp() throws Exception {
+        app = EmbeddedApp.fromServer(TransferService.server(
+                config -> config
+                        .development(true)
+                        .port(0)
+        ));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        app.close();
+    }
+
     @Test
     public void name() throws Exception {
-        try (EmbeddedApp app = EmbeddedApp.fromServer(TransferService.server())) {
-            app.test(client -> {
-                assertThat(client.get("/transfers/asd").getBody().getText()).isEqualTo("Return tx asd");
-            });
-        }
+        app.test(client -> {
+            assertThat(client.get("/transfers/asd").getBody().getText()).isEqualTo("Return tx asd");
+        });
     }
 }
