378c9c8c1d365c9703e8c79e793e7baa4992bd3d
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-02-11 18:37:51 +0000

    Externally modifiable config

--- src/test/java/com/revolut/tests/zkiss/transfersvc/CreateTransferIT.java
+++ src/test/java/com/revolut/tests/zkiss/transfersvc/CreateTransferIT.java
@@ -3,21 +3,23 @@ package com.revolut.tests.zkiss.transfersvc;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ratpack.func.Action;
+import ratpack.server.ServerConfigBuilder;
 import ratpack.test.embed.EmbeddedApp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateTransferIT {
 
+    private static final Action<ServerConfigBuilder> EMBEDDED = config -> config
+            .development(true)
+            .port(0);
+
     private EmbeddedApp app;
 
     @Before
     public void setUp() throws Exception {
-        app = EmbeddedApp.fromServer(TransferService.server(
-                config -> config
-                        .development(true)
-                        .port(0)
-        ));
+        app = EmbeddedApp.fromServer(TransferService.server(EMBEDDED));
     }
 
     @After
