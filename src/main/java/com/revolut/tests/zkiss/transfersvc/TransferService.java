54f0cab964c00de1add04965e17bebab79bf2295
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-02-12 21:31:51 +0000

    dropwizard wip

--- src/main/java/com/revolut/tests/zkiss/transfersvc/TransferService.java
+++ src/main/java/com/revolut/tests/zkiss/transfersvc/TransferService.java
@@ -2,13 +2,23 @@ package com.revolut.tests.zkiss.transfersvc;
 
 import com.revolut.tests.zkiss.transfersvc.config.TransferServiceConfig;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class TransferService extends Application<TransferServiceConfig> {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new TransferService().run(args);
     }
 
+    @Override
+    public String getName() {
+        return "transfer-service";
+    }
+
+    @Override
+    public void initialize(Bootstrap<TransferServiceConfig> bootstrap) {
+        // TODO
+    }
 
     @Override
     public void run(TransferServiceConfig transferServiceConfig, Environment environment) throws Exception {
