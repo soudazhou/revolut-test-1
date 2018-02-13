f038ec34804fe538e8ce7d5891e34b5750dece7b
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-02-13 23:11:02 +0000

    App startup test

---
+++ src/test/java/com/revolut/tests/zkiss/transfersvc/TransferServiceIT.java
@@ -0,0 +1,22 @@
+package com.revolut.tests.zkiss.transfersvc;
+
+import com.revolut.tests.zkiss.transfersvc.config.TransferServiceConfig;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TransferServiceIT {
+
+    @ClassRule
+    public static final DropwizardAppRule<TransferServiceConfig> RULE =
+            new DropwizardAppRule<TransferServiceConfig>(TransferService.class,
+                    ResourceHelpers.resourceFilePath("config.yaml"));
+
+    @Test
+    public void appStartsUp() {
+        assertThat(RULE.getConfiguration().getDataSourceFactory().getUrl()).isNotNull();
+    }
+}
