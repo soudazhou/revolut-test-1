c0775352e1e4ed75392e3387df74c6b7c32022ed
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2018-02-18 20:24:49 +0000

    Hit root in IT

--- src/test/java/com/revolut/tests/zkiss/transfersvc/TransferServiceIT.java
+++ src/test/java/com/revolut/tests/zkiss/transfersvc/TransferServiceIT.java
@@ -6,6 +6,8 @@ import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import javax.ws.rs.core.Response;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TransferServiceIT {
@@ -18,5 +20,8 @@ public class TransferServiceIT {
     @Test
     public void appStartsUp() {
         assertThat(RULE.getConfiguration().getDataSourceFactory().getUrl()).isNotNull();
+
+        Response response = RULE.client().target("http://localhost:" + RULE.getLocalPort() + "/").request().get();
+        assertThat(response.getStatus()).isEqualTo(404);
     }
 }
